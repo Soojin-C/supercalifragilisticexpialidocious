4ce34c89d53bc55047a443124eed274f9f63bcf8
taniacao12 <tcaosu@stuy.edu>
2019-01-09 14:03:38 -0500

    add login

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
+#Team pengWin: Maryann Foley and Tania Cao
+#SoftDev1 pd8
+#K14 -- Do I Know You?
+#2018-10-02    
+
+from flask import Flask, render_template, request,session,url_for,redirect
 import os
-
-from flask import Flask, request, render_template, \
-     flash, session, url_for, redirect
-
-from util import db, info
-
+import csv
 app = Flask(__name__)
 
-app.secret_key = os.urandom(32)
+app.secret_key=os.urandom(32)
 
-#@app.route(/)
+@app.route("/", methods=['POST',"GET"])
+def home():
+	if session.get("uname"):
+		return render_template("welcome.html")
+	return render_template("login.html",Title = 'Login')
+
+@app.route("/auth", methods=['POST'])
+def auth():
+	givenUname=request.form["username"]
+	givenPwd=request.form["password"]
+	if givenUname=="usr": 
+		if givenPwd=="pwd":
+			session["uname"]=givenUname
+			if session.get("error"):
+				session.pop("error")
+		else:
+			session["error"]=2#error 2 means password was wrong
+		return redirect(url_for("home"))
+	else:
+		session["error"]=1
+		return redirect(url_for("home"))#error 1 means username was wrong
+
+@app.route("/logout", methods=['POST',"GET"])
+def logout():
+	if session.get("uname"):
+		session.pop("uname")
+		print(session)
+	return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.debug = True

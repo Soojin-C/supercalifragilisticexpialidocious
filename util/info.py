5fdca863679846accbe07242b9b1dc523a255431
ShinBamba <sbamba@stuy.edu>
2019-01-10 14:03:24 -0500

    getComp aded

--- util/info.py
+++ util/info.py
@@ -1,4 +1,4 @@
-import urllib
+from urllib import request
 import json
 
 #try:
@@ -6,12 +6,19 @@ import json
 #        api_dict = json.load(file)
 #except:
 #    print("Missing api.json")
-
+'''
+Given a company name, returns the company symbols if exists, otherwise returns "NONE"
+'''
 def getCompany(name):
 #    try:
-    iexUrl =  urllib.request.Request("https://api.iextrading.com/1.0/ref-data/symbols", headers={'User-Agent': 'Mozilla/5.0'})
-    data = json.loads(request.urlopen(f2fUrl).read())
-    print(data)
-    return data
+    iexUrl = request.Request("https://api.iextrading.com/1.0/ref-data/symbols", headers={'User-Agent': 'Mozilla/5.0'})
+    data = json.loads(request.urlopen(iexUrl).read())
+    symbols = {}
+    for comp in data:
+        symbols[comp["name"]] = comp["symbol"]
+    if name in symbols:
+        return symbols[name]
+    else:
+        return "NONE"
 
-print(getCompany("yes"))
+#print(getCompany("Nymox Pharmaceutical Corporation"))
